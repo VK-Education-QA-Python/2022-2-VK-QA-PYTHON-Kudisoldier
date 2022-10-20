1296dd46154148c213ccdc0ba4353e6851082165
Efim <evim_l@bk.ru>
2022-10-20 15:14:25 +0300

    add allure report and selenoid support

--- README.md
+++ README.md
@@ -18,13 +18,41 @@ Contents:
 Steps to run homework2:
 ```
 cd homework 2
-pytest -v -m UI --headless -n 3
+pytest -v -m UI --headless -n 3 --alluredir allure
+```
+Steps to view allure report:
+```
+allure serve allure
+```
+Steps to run selenoid:
+```
+add --selenoid option to pytest
+./cm_darwin_amd64 selenoid-ui start
+
+pytest --selenoid -n 3
+
+OR for my mac m1
+
+docker run -d                                   \
+--name selenoid                                 \
+-p 4444:4444                                    \
+-v /var/run/docker.sock:/var/run/docker.sock    \
+-v `pwd`/config/:/etc/selenoid/:ro              \
+--privileged                                    \
+aerokube/selenoid:latest-release                \
+-service-startup-timeout 1m
+
+docker run -d         \
+    --name selenoid-ui  \
+    --link selenoid     \
+    -p 8080:8080        \
+    aerokube/selenoid-ui --selenoid-uri=http://selenoid:4444
 ```
 
 Contents:
 - advertising campaign test
-- creation of segment with audience in app
-- creation of segment with vk edu group
+- creation of segment with audience in app test
+- creation of segment with vk edu group test
 
 Features:
 - runnable in selenoid
--- homework2/conftest.py
+++ homework2/conftest.py
@@ -6,31 +6,57 @@ from selenium.webdriver.chrome.options import Options
 import utils
 from api import api_client
 from pages.main_page import MainPage
+import allure
 
 
 def pytest_addoption(parser):
     parser.addoption('--headless', action='store_true')
+    parser.addoption('--selenoid', action='store_true')
+    parser.addoption('--vnc', action='store_true')
 
 
 @pytest.fixture()
 def config(request):
     headless = request.config.getoption("--headless")
-    return {"headless": headless}
+    if request.config.getoption('--selenoid'):
+        if request.config.getoption('--vnc'):
+            vnc = True
+        else:
+            vnc = False
+        selenoid = 'http://127.0.0.1:4444/wd/hub'
+    else:
+        selenoid = None
+        vnc = False
+    return {'headless': headless, 'vnc': vnc, 'selenoid': selenoid}
 
 
+@allure.title("Driver setup")
 @pytest.fixture()
 def driver(config):
     options = Options()
     options.headless = config["headless"]
+    if config['selenoid']:
+        capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "91.0",
+            "selenoid:options": {
+                "enableVideo": False
+            }
+        }
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="105.0.5195.19").install()),
-                              options=options)
+        driver = webdriver.Remote(
+            command_executor=config['selenoid'],
+            desired_capabilities=capabilities)
+    else:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="105.0.5195.19").install()),
+                                  options=options)
     driver.set_window_size(1280, 720)
 
     yield driver
     driver.close()
 
 
+@allure.title("Dashboard page init")
 @pytest.fixture()
 def dashboard_page(driver):
     email, password = utils.get_credentials()
@@ -40,4 +66,11 @@ def dashboard_page(driver):
     utils.driver_load_requests_session(driver, requests_session)
     dashboard_page = main_page.go_to_dashboard()
 
-    return dashboard_page
+    yield dashboard_page
+
+    res = ''
+    for i in driver.get_log('browser'):
+        res += f"{i['level']} - {i['source']}\n{i['message']}\n"
+    allure.attach(res, name="browser log", attachment_type=allure.attachment_type.TEXT)
+
+
