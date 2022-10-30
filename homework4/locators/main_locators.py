00accee4abb23149684adc81d167828a94d1be75
Efim <evim_l@bk.ru>
2022-10-30 21:23:38 +0300

    add news source test

--- homework4/locators/main_locators.py
+++ homework4/locators/main_locators.py
@@ -8,3 +8,7 @@ class MainLocators:
     SUGGESTED_ITEMS = (AppiumBy.ID, "ru.mail.search.electroscope:id/item_suggest_text")
     VOICE_ASSISTANT_BUTTON = (AppiumBy.ID, "ru.mail.search.electroscope:id/assistant_voice_input_group")
     FACT_CARD_TITLE = (AppiumBy.ID, "ru.mail.search.electroscope:id/item_dialog_fact_card_title")
+    CHAT_MESSAGE = (AppiumBy.ID, "ru.mail.search.electroscope:id/dialog_item")
+    SETTINGS_BUTTON = (AppiumBy.ID, "ru.mail.search.electroscope:id/assistant_menu_bottom")
+    AUDIO_TRACK = (AppiumBy.ID, "ru.mail.search.electroscope:id/player_track_name")
+    PLAY_BUTTON = (AppiumBy.ID, "ru.mail.search.electroscope:id/play_button")
