2f78033fd60013816e5ed5f9913eb0062214da2a
Efim <evim_l@bk.ru>
2022-10-30 22:50:50 +0300

    add settings test

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ Contents:
 - 2 login negative test
 - change contacts test
 - 2 parametrized menu page navigation
+
+## Homework 4 - testing android UI with appium
+Steps to run homework4:
+```
+run phone emulator with Adnroid Studio
+run appium server with settings
+cd homework 4
+pytest -v -m AndroidUI
+```
+
+Contents:
+- command window test
+- calculator test
+- news source test
+- settings test
--- homework4/test_android.py
+++ homework4/test_android.py
@@ -1,13 +1,9 @@
-import pytest
-import time
-
-@pytest.mark.skip
 def test_command_window(main_page):
     main_page.search_text("Russia")
     main_page.swipe_suggested("население россии")
     assert main_page.get_fact_result() == '146 млн.'
 
-@pytest.mark.skip
+
 def test_calculator(main_page):
     main_page.search_text('2+2*2')
     assert int(main_page.get_chat_result()) == (2+2*2)
@@ -23,5 +19,12 @@ def test_news_source(main_page):
     main_page = settings_page.close()
     main_page.search_text('News')
     assert main_page.get_audio_name() == 'Новости Mail.ru'
+
+
+def test_about_setting(main_page, version):
+    settings_page = main_page.open_settings()
+    about_page = settings_page.open_about()
+    assert about_page.get_app_version() == version
+    assert about_page.get_app_copyright() == 'Mail.ru Group © 1998–2022. Все права защищены.'
 
 
